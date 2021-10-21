2c74b61868bdbc33f1968328f99b620d6c0ea848
Abdul Kareem <abdulkareem786687@gmail.com>
2021-10-22 01:04:45 +0530

    Add Navigator for phonebook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import "./App.css"
 
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import allReducers from "./reducers";
+import Recent from "./components/Recent";
+import PhoneBook from "./components/PhoneBook";
+import Favorite from "./components/Favorite";
+
+const store = createStore(allReducers);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [value, setValue] = React.useState(1);
+    return (
+        <Provider store={store}>
+            <div className = "main">
+                <Box sx={{ maxWidth: 700,margin:"auto" }}>
+                    <BottomNavigation
+                        showLabels
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                            console.log(newValue);
+                        }}
+                    >
+                        <BottomNavigationAction
+                            label="Recents"
+                            icon={<RestoreIcon />}
+                        />
+                            <BottomNavigationAction
+                                label="PhoneBook"
+                                icon={<LocationOnIcon />}
+                            />
+                        <BottomNavigationAction
+                            label="Favorites"
+                            icon={<FavoriteIcon />}
+                        />
+                        
+                    </BottomNavigation>
+                </Box>
+                {value === 0? <Recent/>:(value === 1?<PhoneBook/>:<Favorite/>)}
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
---
+++ src/components/PhoneBook.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PhoneBook() {
+    return (
+        <div>
+            <h1>PhoneBook</h1>
+        </div>
+    )
+}
+
+export default PhoneBook
